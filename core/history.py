a02fe93ea67426a6f351b3da82583806f6229b89
Lucas Pimentel <lucas.pimentel@estudante.ifmt.edu.br>
2025-08-02 01:59:50 -0400

    criando a classe histórico

---
+++ core/history.py
@@ -0,0 +1,10 @@
+import pygame
+
+class History:
+    def __init__(self):
+        self.module = None
+        self.questions = []
+        self.qtd_questions = 0
+        self.level = None
+        self.hits = None
+        self.xp = None
